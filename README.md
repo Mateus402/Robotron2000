f6b92cbe6b2e39f1bb40943d60b043989a6a5e9e
Mateus Stangherlin <mateus_stangherlin@hotmail.com>
2023-05-02 14:09:10 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# robotron-2000
+
+Projeto realizado com foco em JavaScript através do curso de JavaScript: manipulando o DOM realizado na plataforma Alura.
---
+++ js/main.js
@@ -0,0 +1,58 @@
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        minupularDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatiscticas(evento.target.dataset.peca)
+    })
+})
+
+function minupularDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]')
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatiscticas(peca) {
+    estatisticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
+    })
+}
